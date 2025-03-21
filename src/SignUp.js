a84bf0c1a3141f4b27e303e62f9aa6d595f5d0b5
Tahir Kamal <109749722+GoodGuycoding@users.noreply.github.com>
2025-03-21 03:54:46 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="SignUp">
-      <h1>SIGN UP</h1>
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <input
+          style={{ marginBottom: "20px" }}
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          style={{ marginBottom: "20px" }}
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <button className="submitBtn">Submit</button>
+      </form>
     </div>
   );
 }
